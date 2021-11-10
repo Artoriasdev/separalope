d814dc8e27ded51a8321ec7430e379c1126fbc1b
Hirikari1 <piero.bryan@hotmail.com>
2021-11-10 14:38:13 -0500

    Refactorización : Cambio de imagen trabaja con nosotros

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -18,6 +18,11 @@ import {
 } from "@material-ui/core";
 import { Search } from "@material-ui/icons";
 import { ArrowLeftSVG, ArrowRightSVG } from "../assets/images/svg";
+import trb1 from "../assets/images/trabaja con nosotros 1325 x 267.jpg";
+import trb2 from "../assets/images/trabaja con nosotros 1325 x 325.jpg";
+import trb3 from "../assets/images/Transforma tu negocio 273 x 236.jpg";
+import trb4 from "../assets/images/Transforma tu negocio 273 x 271.jpg";
+
 // import FullPageLoader from "./FullPageLoader";
 
 const responsive = {
@@ -321,32 +326,59 @@ class HomePage extends Component {
                 )}
             </CarouselItem>
           </div>
-
-          <a
-            href="https://wa.link/oki91a"
-            target="_blank"
-            rel="noreferrer"
-            style={{
-              cursor: "pointer",
-              marginLeft: "50px",
-              marginRight: "50px",
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <img
-              src="https://www.qapaq.pe/wp-content/uploads/2020/06/Trabaja-con-Nosotros-1-51-960x250.png"
-              alt="Trabaja con nosotros"
-              title="Trabaja con nosotros"
+          {window.innerWidth > 1200 ? (
+            <a
+              href="https://wa.link/oki91a"
+              target="_blank"
+              rel="noreferrer"
               style={{
-                width: "100%",
-                height: "100%",
-                marginTop: "20px",
-                maxWidth: "1325px",
-                maxHeight: "325px",
+                cursor: "pointer",
+                marginLeft: "50px",
+                marginRight: "50px",
+                display: "flex",
+                justifyContent: "center",
               }}
-            />
-          </a>
+            >
+              <img
+                src={trb2}
+                alt="Trabaja con nosotros"
+                title="Trabaja con nosotros"
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  marginTop: "20px",
+                  maxWidth: "1325px",
+                  maxHeight: "325px",
+                }}
+              />
+            </a>
+          ) : (
+            <a
+              href="https://wa.link/oki91a"
+              target="_blank"
+              rel="noreferrer"
+              style={{
+                cursor: "pointer",
+                marginLeft: "50px",
+                marginRight: "50px",
+                display: "flex",
+                justifyContent: "center",
+              }}
+            >
+              <img
+                src={trb4}
+                alt="Trabaja con nosotros"
+                title="Trabaja con nosotros"
+                style={{
+                  width: "270px",
+                  height: "100%",
+                  marginLeft: window.innerWidth <= 360 ? "20px" : "0",
+                  marginTop: "20px",
+                  maxHeight: "325px",
+                }}
+              />
+            </a>
+          )}
         </div>
       </div>
     );
